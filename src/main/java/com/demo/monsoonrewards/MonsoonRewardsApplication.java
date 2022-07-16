57b8b6a601b9b53803788b80f1b2a8786fbc49bc
Saurabh Agrawal <saurabh.cse.95@outlook.com>
2022-07-16 17:29:48 +0530

    initial-commit

---
+++ src/main/java/com/demo/monsoonrewards/MonsoonRewardsApplication.java
@@ -0,0 +1,13 @@
+package com.demo.monsoonrewards;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MonsoonRewardsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MonsoonRewardsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/demo/monsoonrewards/MonsoonRewardsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demo.monsoonrewards;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MonsoonRewardsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
